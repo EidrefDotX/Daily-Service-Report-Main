7759c289f6dcea25f5f1483fd19f31d6db7443cd
EidrefDotX <eidref.dotx@gmail.com>
2025-11-17 13:27:25 +0800

    Delete Daily-Service-Report-main directory

--- Daily-Service-Report-main/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting Daily Service Report Backend..."
-echo ""
-echo "Creating default engineer users (EN001-EN009)"
-echo "Password: #DotXsolutions.opc"
-echo ""
-python app.py
-
